8aac76772b225aa10648328ee267d9ecd20c60c3
OxyMonster <giorgizeikidze97@gmail.com>
2019-04-08 16:54:32 +0400

    soundApp

---
+++ index.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', ()=>{
+    const sounds = document.querySelectorAll('.sound');
+    const pads = document.querySelectorAll('.pads div');
+    const visual = document.querySelector('.visual');
+    const colors = [
+        '#60d394',
+        '#d36060',
+        '#c060d3',
+        '#d3d160',
+        '#6860d3',
+        '#60b2d3'
+
+    ]
+    
+
+// Lets get going witht he sounds from here 
+    pads.forEach((pad,index) =>{
+        pad.addEventListener('click', function(){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            createBubbles(index);
+            console.log(keypress);
+        })
+    });
+    //Create function that makes bubbles
+    const createBubbles = (index) =>{
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationed', function(){
+            visual.removeChild(this);
+        })
+    }
+});
+
